f19d5c3ef252d8ee6f5f2095c4550f5d6c8f97c5
Kasia Iwańska <sobylmine@gmail.com>
2016-07-12 12:41:44 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function(){
 
-	var blocks = $('.block'),
+	var blocks = document.querySelectorAll('.block'),
+		active = document.querySelectorAll('.active'),
 		mycolors = ['#E91E63', '#303F9F', '#2196F3', '#4CAF50', '#009688', '#FFC107', '#00BCD4', '#5D6D7E', '#884EA0', '#F8BBD0', '#F44336', '#CDDC39'],
 		content = [],
+		activeBlocks = [],
 		active = 0,
 		points = 0,
 		moves = 0;
-
 
 	function createMemoBlocks() {
 		for (var i = 0; i < mycolors.length; i++) {
@@ -19,7 +20,8 @@ $(document).ready(function(){
 		}
 		shuffle(content);
 		for (var i = 0; i < content.length; i++) {
-			$(blocks[i]).attr('data-color', content[i].color).attr('data-id', content[i].id);
+			blocks[i].dataset.color = content[i].color;
+			blocks[i].dataset.id = content[i].id;
 		}
 	}
 
@@ -38,46 +40,54 @@ $(document).ready(function(){
 	}
 
 	createMemoBlocks();
-	
+	console.log(blocks);
 
-	blocks.hover(function() {
-		$(this).toggleClass('ready');
-	  }, function() {
-	  	$(this).toggleClass('ready');
-	  }
-	);
+	for (var i = 0; i < blocks.length; i++) {
 
-	blocks.on('click', function(){
-		$(this).toggleClass('ready').toggleClass('active').css('background-color', $(this).data('color'));
-		active += 1;
-		if (active === 2) {
-			if ($('.active').eq(0).data('id') === $('.active').eq(1).data('id')) {
-				setTimeout(hit, 500);
-				active = 0;
-				points += 1;
-				moves += 1;
+		blocks[i].addEventListener('click', function(){
+			this.classList.remove('ready');
+			this.classList.add('active');
+			this.style.backgroundColor = this.dataset.color;
+			active ++;
+			activeBlocks = document.querySelectorAll('.active');
+			if (active === 2) {
+				if (activeBlocks[0].dataset.id === activeBlocks[1].dataset.id) {
+					setTimeout(hit, 500);
+					active = 0;
+					points ++;
+					moves ++;
+				}
+				else {
+					setTimeout(missed, 500);
+					active = 0;
+					moves ++;
+				}
 			}
-			else {
-				setTimeout(missed, 500);
-				active = 0;
-				moves += 1;
+			if (points === 12) {
+			 	var again = document.querySelector('#again');
+			 	again.innerText = 'You made it! And it tooked you '+ moves +' moves. Try again and be better!';
+			 	again.classList.remove('hidden');
 			}
-		}
-		if (points === 12) {
-			$('#again').text('You made it! And it tooked you '+ moves +' moves. Try again and be better!').toggleClass('hidden');
-		}
-	});
+		});
+	}
 
-	$('#again').on('click', function(){
+	document.querySelector('#again').addEventListener('click', function(){
 		location.reload(true);
 	});
 
-	function missed() {		
-		$('.active').css('background-color', '#ffffff').toggleClass('active');
+	function missed() {	
+		console.log(activeBlocks);	
+		for (var i = 0; i < activeBlocks.length; i++) {
+			activeBlocks[i].style.backgroundColor = '#ffffff';
+			activeBlocks[i].classList.remove('active');
+		}
 	}
 
 	function hit() {
-		$('.active').toggleClass('active').toggleClass('gone');
+		for (var i = 0; i < activeBlocks.length; i++) {
+			activeBlocks[i].classList.remove('active');
+			activeBlocks[i].classList.add('gone');
+		}
 	}
 
 });
